f90377868e8f232d4fba4be3ddda7b91a831cfdd
DuckyJN <jnguye74@gmail.com>
2018-10-14 17:19:23 -0400

    Added headers in application.css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
-  has_many :comments, dependent: :destroy
-
+  has_many :comments, dependent: :destroygit 
 
   def index
     @articles = Article.all
